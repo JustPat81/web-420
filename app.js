10dd6af9774a7596e2578e12cdb7dab8a75c8b4b
Patrick Wolff <pat.wolff81@gmail.com>
2022-08-14 16:00:19 -0500

    Adding Assignment 1.2

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const mongoose = require('mongoose');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({'extended': true}));
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'WEB 420 RESTful APIs',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.js'], // files containing annotations for the OpenAPI Specification
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log(`Application started and listening on port ${app.get('port')}`);
+})
